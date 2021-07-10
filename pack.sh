9bfb256914c8a97e0a2268edf83eeffcaef846a7
AstronIsTaken <vasily.lementar@gmail.com>
2021-07-10 17:50:32 -0400

    Remove extras

--- pack.sh
+++ pack.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
-
-while getopts ":rp" opt; do
-  case ${opt} in
-    r ) # release (public zip)
-        rm du-map.zip
-        zip -r du-map.zip ./* -x "pack.sh" -x "README.md" -x ".*" -x "config.php" -x "TODO" -x "*.php" -x "config.php.example"
-        exit 0
-      ;;
-    p ) # production (Website)
-        rm du-map.zip
-        zip -r du-map.zip ./* -x "pack.sh" -x "README.md" -x ".*" -x "config.php" -x "TODO" -x "*.php" -x "config.php.example" -x "*.bak"
-        exit 0
-      ;;
-    \? ) echo "Usage: pack.sh [-r] [-p]"
-      ;;
-  esac
-done
+rm du-map.zip
+zip -r du-map.zip ./* -x "pack.sh" -x "README.md" -x ".*" -x "config.php" -x "TODO" -x "*.php" -x "config.php.example" -x "*.bak"
 
